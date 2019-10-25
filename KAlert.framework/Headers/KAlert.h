c56c7d3fc0f56cd49bd58a063e6890bdb49511f8
Krishna Datt Shukla <krishnads03@gmail.com>
2019-10-25 10:30:48 +0530

    Added Framework For easy implementation

---
+++ KAlert.framework/Headers/KAlert.h
@@ -0,0 +1,19 @@
+//
+//  KAlert.h
+//  KAlert
+//
+//  Created by Apple on 15/10/19.
+//  Copyright © 2019 Konstant info Solutions Pvt. Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KAlert.
+FOUNDATION_EXPORT double KAlertVersionNumber;
+
+//! Project version string for KAlert.
+FOUNDATION_EXPORT const unsigned char KAlertVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KAlert/PublicHeader.h>
+
+
